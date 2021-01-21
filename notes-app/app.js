0c35ebdaaa1c6a86acf0361ee0ded240aa5c9f1f
Tauqeer Ahmed <tauqeer.shakir@yahoo.com>
2021-01-21 14:09:03 +0500

    added notes.js file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
-
+const addFunction = require('./utils');
+const getNotes = require('./notes');
 // fs.writeFileSync('notes.txt', 'Test file 1');
 
-fs.appendFileSync('notes.txt', 'append the text');
+// fs.appendFileSync('notes.txt', 'append the text');
+console.log(addFunction(2, 3));
+
+console.log(getNotes());
